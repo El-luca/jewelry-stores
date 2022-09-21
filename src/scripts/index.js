48be17e19c63fd7a729ee528aa1702425f124a8e
LUCAS NASCIMENTO DA SILVA <93679314+El-luca@users.noreply.github.com>
2022-09-21 03:02:01 -0300

    add hover in menu
    add hover in all menus (missed background in menu items)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,105 +1,130 @@
-function constructInformativesTop(json){
-    const $wrapper = document.querySelector('.header__informatives-wrapper')
-    for (completeText of json){
-        $wrapper.innerHTML += `
-        <span class="header__informatives-top--text${completeText.firstBold ? "-strong" : ""}">
+function constructInformativesTop(json) {
+  const $wrapper = document.querySelector(".header__informatives-wrapper");
+  for (completeText of json) {
+    $wrapper.innerHTML += `
+        <span class="header__informatives-top--text${
+          completeText.firstBold ? "-strong" : ""
+        }">
           ${completeText.text}
-          <strong class="header__informatives-top--text${completeText.firstBold == false ? "-strong" : "-margin"}">
+          <strong class="header__informatives-top--text${
+            completeText.firstBold == false ? "-strong" : "-margin"
+          }">
             ${completeText.bold}
           </strong>
-        </span>`
-    }
+        </span>`;
+  }
 }
 
-function informativesTop(){
-    fetch("../src/mocks/INFORMATIVES__TOP.json").then(function (response){
-        return response.json()
-    }).then(function(json){
-        constructInformativesTop(json)
+function informativesTop() {
+  fetch("../src/mocks/INFORMATIVES__TOP.json")
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (json) {
+      constructInformativesTop(json);
+    });
 }
 
-function requestMenuDesktop(){
-    fetch("../../src/mocks/MENU.json").then(function (response){
-        return response.json();
-    }).then(function (json){
-        constructMenuDesktop(json.menu)
+function requestMenuDesktop() {
+  fetch("../../src/mocks/MENU.json")
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (json) {
+      constructMenuDesktop(json.menu);
+    });
 }
- 
-function constructMenuDesktop(json){
-    let structureMenu = "";
-    const $containerMenu =  document.querySelector('.header__menu')
-    for (const menu of json){
-        structureMenu += `<div class="header__menu-wrapper-category">
-        <a href="${menu.url}" class="header__menu-department">${menu.name}</a>
-        ${menu.children ? `      <div class="header__menu-items">
-        ${menu.children.map(function (child){
-            return`
+
+function constructMenuDesktop(json) {
+  let structureMenu = "";
+  const $containerMenu = document.querySelector(".header__menu");
+  for (const menu of json) {
+    structureMenu += `<div class="header__menu-wrapper">
+        <a href="${menu.url}" class="header__menu-department script">${
+      menu.name
+    }</a>
+        ${
+          menu.children
+            ? `<div class="header__menu-items ${
+              menu.name === "Turmalina" ? "turmalina" : ""
+            }">
+        ${menu.children.map(function (child) {
+          return `
             <div class="header__menu-wrapper-category">
                 <a href=${child.url} class="header__menu-category">
                     ${child.name}
                 </a>
                 
-                ${child.children ? `
-                <div class="header__menu-sub-category">
-                ${child.children.map(function(subchild){
-                    return`
+                ${
+                  child.children
+                    ? `
+                  <div class="header__menu-sub-category">
+                    ${child.children.map(function (subchild) {
+                      return `
                         <a href=${subchild.url} class="header__menu-category">
                             ${subchild.name}
                         </a>
-                    `
-                })}                    
-                </div>
-                ` : ``}
+                      `;
+                    })}                    
+                  </div>
+                `
+                    : ``
+                }
             </div>
-            `
+            `;
         })}
-        </div>` : ""}
         </div>`
-    }
+            : ""
+        }
+        </div>`;
+  }
 
-    $containerMenu.innerHTML = structureMenu;
+  $containerMenu.innerHTML = structureMenu;
 }
 
-// informativesTop()
-requestMenuDesktop()
+informativesTop();
+requestMenuDesktop();
 
-function handleToggleMenuMobile(){
-    const $menuMobile = document.querySelector('.header__menu-hamburguer')
-    const $esctrutureMenuMobile = document.querySelector('.header__menu-mobile')
-    const $closeMenuImgX = document.querySelector('.close-menu')
-    const $shadowCloseMenuMobile = document.querySelector('.header__menu-mobile-shadow')
+function handleToggleMenuMobile() {
+  const $menuMobile = document.querySelector(".header__menu-hamburguer");
+  const $esctrutureMenuMobile = document.querySelector(".header__menu-mobile");
+  const $closeMenuImgX = document.querySelector(".close-menu");
+  const $shadowCloseMenuMobile = document.querySelector(
+    ".header__menu-mobile-shadow"
+  );
 
-    $menuMobile.addEventListener("click", function(){
-        $esctrutureMenuMobile.classList.add("active")
-  
-    })
-    $closeMenuImgX.addEventListener("click", function(){
-        $esctrutureMenuMobile.classList.remove("active")
-    })
-    $shadowCloseMenuMobile.addEventListener("click", function(){
-        $esctrutureMenuMobile.classList.remove('active')
-    })
+  $menuMobile.addEventListener("click", function () {
+    $esctrutureMenuMobile.classList.add("active");
+  });
+  $closeMenuImgX.addEventListener("click", function () {
+    $esctrutureMenuMobile.classList.remove("active");
+  });
+  $shadowCloseMenuMobile.addEventListener("click", function () {
+    $esctrutureMenuMobile.classList.remove("active");
+  });
 }
 
-handleToggleMenuMobile()
+handleToggleMenuMobile();
 
-function handleMenuMobileCategory(){
-    const $iconShowDepartament = document.querySelector(".icon-menu-mobile-arrow")
-    const $menuCategory = document.querySelector('.header__menu-mobile-itens')
-    $iconShowDepartament.addEventListener('click', function (){
-        $menuCategory.classList.toggle('active')
-    })
+function handleMenuMobileCategory() {
+  const $iconShowDepartament = document.querySelector(
+    ".icon-menu-mobile-arrow"
+  );
+  const $menuCategory = document.querySelector(".header__menu-mobile-itens");
+  $iconShowDepartament.addEventListener("click", function () {
+    $menuCategory.classList.toggle("active");
+  });
 }
 
-handleMenuMobileCategory()
+handleMenuMobileCategory();
 
-function rotateArrow(){
-    const $iconShowDepartament = document.querySelector(".icon-menu-mobile-arrow")
-    $iconShowDepartament.addEventListener('click', function(){
-        $iconShowDepartament.classList.toggle('active')
-    })
+function rotateArrow() {
+  const $iconShowDepartament = document.querySelector(
+    ".icon-menu-mobile-arrow"
+  );
+  $iconShowDepartament.addEventListener("click", function () {
+    $iconShowDepartament.classList.toggle("active");
+  });
 }
 
-rotateArrow()
+rotateArrow();
